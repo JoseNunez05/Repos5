4c9c6c0e81941a65425f8a6b0dd012ae67ed9a0c
JoseNunez05 <josenunez3575@gmail.com>
2023-08-24 16:46:15 -0400

    added customCommands file

---
+++ cypress/e2e/customCommands.cy.js
@@ -0,0 +1,20 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('https://example.cypress.io')
+
+      Cypress.Commands.add('clickActionsLink', (test1, test2) => {
+        cy.get('ul.home-list>li>a')
+          .contains('Actions')
+          .click()
+      });
+
+      Cypress.Commands.add('typeMyName', (p1, p2) => {
+        cy.get('input#email1.form-control.action-email')
+          .type('Jose Nunez')
+      });
+
+      cy.clickActionsLink()
+      cy.typeMyName()
+
+    })
+  })
--- cypress/e2e/test_spec.cy.js
+++ cypress/e2e/test_spec.cy.js
@@ -1,8 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://demoqa.com/alerts')
-
-    
-
+    cy.visit('https://example.cypress.io')
   })
 })
