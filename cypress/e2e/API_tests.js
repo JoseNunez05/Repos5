796af8923e6f15fd30faca35ddd4da4537f78d70
JoseNunez05 <josenunez3575@gmail.com>
2023-08-25 15:37:51 -0400

    nothing much

--- cypress/e2e/API_tests.js
+++ cypress/e2e/API_tests.js
@@ -1,8 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://demoqa.com/alerts')
+// describe('API Testing with Cypress', () => {
+//   it('should retrieve all booking IDs', () => {
+//     cy.request('GET', 'https://restful-booker.herokuapp.com/booking')
+//       .should((response) => {
+//         expect(response.status).to.eq(200);
+//         expect(response.body).to.have.length.greaterThan(0);
+//       });
+//   });
+// });
 
-    
 
-  })
-})
+// describe("Data-Driven API Testing with Cypress", () => {
+//   const testFirstNames = ["John", "Sally", "Michael"];
+//   testFirstNames.forEach((firstName) => {
+//     it(`Retrieves booking IDs for ${firstName}`, () => {
+//       cy.request(`GET`, `https://restful-booker.herokuapp.com/booking?firstname=${firstName}`).then((response) => {
+//         expect(response.status).to.eq(200);
+//         expect(response.body).to.have.length.greaterThan(0);
+//         response.body.forEach((booking) => {
+//           expect(booking.firstname).to.eq(firstName);
+//         });
+//       });
+//     });
+//   });
+// });
+
+
