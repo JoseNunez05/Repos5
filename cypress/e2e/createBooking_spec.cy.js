bcec680467d7883571d6c44d79169603e0164d33
JoseNunez05 <josenunez3575@gmail.com>
2023-08-28 18:21:42 -0400

    created booking spec. post request failed.

--- cypress/e2e/createBooking_spec.cy.js
+++ cypress/e2e/createBooking_spec.cy.js
@@ -1,25 +1,57 @@
-describe('POST Request', ()=> {
-    it('Post Call', ()=>{
+describe('HTTP Requests', ()=> {
+
+    it('GET thing', ()=> {
+      cy.request('GET', 'https://restful-booker.herokuapp.com/apidoc/index.html')
+        .its('status')
+        .should('equal', 200);
+    })
+
+        it('Post Call', ()=>{
         cy.request({
 
-            method: 'POST',
-            url:    'https://restful-booker.herokuapp.com/booking',
-            body: {
-                    firstname: "Jim",
-                    lastname: "Brown",
-                    totalprice : 111,
-                    depositpaid : true,
-                    bookingdates : {
-                        checkin: "2018-01-01",
-                        checkout: "2019-01-01"
-                    },
-
-                    additionalneeds: "Breakfast"
-                  }
+                method: 'POST',
+                url:    'https://restful-booker.herokuapp.com/booking',
+                body: {
+                        firstname: "Jim",
+                        lastname: "Brown",
+                        totalprice : 111,
+                        depositpaid : true,
+                        bookingdates : {
+                            checkin: "2018-01-01",
+                            checkout: "2019-01-01",
+                        },
+                      }
 
             .its('status')
-            .should('equal', 200);
+            .should('equal', 200),
 
         })
     })
 })
+
+
+// describe('POST Request', ()=> {
+//     it('Post Call', ()=>{
+//         cy.request({
+
+//             method: 'POST',
+//             url:    'https://restful-booker.herokuapp.com/booking',
+//             body: {
+//                     firstname: "Jim",
+//                     lastname: "Brown",
+//                     totalprice : 111,
+//                     depositpaid : true,
+//                     bookingdates : {
+//                         checkin: "2018-01-01",
+//                         checkout: "2019-01-01"
+//                     },
+
+//                     additionalneeds: "Breakfast"
+//                   }
+
+//             .its('status')
+//             .should('equal', 200)
+
+//         })
+//     })
+// })
