d65ffcb4328786f01967b112e68afe3620ed4439
JoseNunez05 <josenunez3575@gmail.com>
2023-08-28 17:37:45 -0400

    health check done

--- cypress/e2e/healthcheck_spec.cy.js
+++ cypress/e2e/healthcheck_spec.cy.js
@@ -1,5 +1,53 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+// checking the status of the website in general
+describe('HTTP Requests', ()=> {
+    it('GET thing', ()=> {
+      cy.request('GET', 'https://restful-booker.herokuapp.com/apidoc/index.html')
+        .its('status')
+        .should('equal', 200);
+    })
 })
+
+
+
+// describe('API CALL', ()=> {
+
+//   it('GET Call', ()=> {
+
+//     cy.request('GET', 'https://restful-booker.herokuapp.com/ping')
+//       .its('status')
+//       .should('equal', 201);
+
+//   })
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://restful-booker.herokuapp.com/apidoc/index.html')
+//   })
+// })
